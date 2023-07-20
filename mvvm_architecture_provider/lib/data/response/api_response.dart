032afc5bbfc4672fad4e2b97e4a43a6b4dd9828d
Muhammad Ibrar <muhammad49ibrar@gmail.com>
2023-07-20 15:06:54 +0500

    mvvm architecture provider

---
+++ mvvm_architecture_provider/lib/data/response/api_response.dart
@@ -0,0 +1,24 @@
+
+import 'package:mvvm_architecture_provider/data/response/status.dart';
+
+class ApiResponse<T> {
+
+  Status? status;
+  String? message;
+  T? data;
+
+  ApiResponse(this.status , this.message , this.data);
+
+  ApiResponse.loading() : status = Status.LOADING;
+
+  ApiResponse.completed() : status = Status.COMPLETED;
+
+  ApiResponse.error() : status = Status.ERROR;
+
+  @override
+  String toString() {
+
+    return "Status : $status \n Message : $message \n Data : $data";
+  }
+
+}
